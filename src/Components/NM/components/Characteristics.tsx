23e4f244d6e392d5f5218662a78258703127ce5b
hoshikira18 <khuyen.dev183@gmail.com>
2025-06-25 01:13:42 +0700

    my code fixed itself

--- src/Components/NM/components/Characteristics.tsx
+++ src/Components/NM/components/Characteristics.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef, useState } from 'react';
 import { faArrowRight, faPlay } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useVisibleOnScroll } from '../hooks/useVisibleOnScroll';
@@ -9,11 +8,11 @@ function Characteristics() {
 		<div className="flex flex-col items-center relative overflow-hidden">
 			<div ref={ref}>
 				<img
+					alt="hanh-fuck"
 					src="/lam-voi-dam-me.png"
 					className={`transition-opacity duration-700 animation-move-left w-52 ${
 						visible ? 'animation-move-left' : 'opacity-0'
 					}`}
-					alt=""
 				/>
 			</div>
 
@@ -50,6 +49,7 @@ const Content = ({
 			</div>
 			<div className="mt-6">
 				<img
+					alt="hanh-fuck"
 					src="/content-green.webp"
 					className="w-[380px] h-[200px]"
 				/>
@@ -81,8 +81,8 @@ const Feature1 = () => {
 			<div className="absolute mt-4 translate-x-[14em] md:translate-x-0">
 				<div className="md:block hidden">
 					<img
+						alt="hanh-fuck"
 						src="/bean.png"
-						alt="cf-bean"
 						loading="lazy"
 						className={
 							visible
@@ -107,8 +107,8 @@ const Feature1 = () => {
 				</div>
 				<div className="translate-x-[-4em] md:hidden block">
 					<img
+						alt="hanh-fuck"
 						src="/bean.png"
-						alt="cf-bean"
 						loading="lazy"
 						className={
 							visible
@@ -140,34 +140,35 @@ const Feature1 = () => {
 					<picture>
 						<div className="-rotate-[10deg]">
 							<img
+								alt="hanh-fuck"
 								className={`transition-opacity duration-700 absolute z-1 scale-115 -top-8 left-36 w-12 ${
 									visible
 										? 'animation-move-right'
 										: 'opacity-0'
 								}`}
 								src="https://www.nescafe.com/vn/themes/custom/nescafe/UI/build/images/zGbszVP.svg"
-								alt="line"
 								loading="lazy"
 							/>
 						</div>
 
 						<img
+							alt="hanh-fuck"
 							className={`transition-opacity duration-700 absolute right-[8em] top-72 z-0 w-36 ${
 								visible ? 'animation-move-right' : 'opacity-0'
 							}`}
 							src="/1.png"
-							alt="nông dân"
 							loading="lazy"
 						/>
 						<img
+							alt="hanh-fuck"
 							className="absolute left-[-2em] top-14 z-[-1] -rotate-3 scale-150 h-64 w-44 border-4 border-white shadow-lg"
 							src="/anh-1.jpg"
-							alt="nông dân"
 							loading="lazy"
 						/>
 					</picture>
 					<div className="w-[360px]">
 						<img
+							alt="hanh-fuck"
 							className={`transition-opacity duration-700 absolute z-[-4] top-28 left-[-12em] scale-150 ${
 								visible ? 'animation-move-left' : 'opacity-0'
 							}`}
@@ -199,8 +200,8 @@ const Feature2 = () => {
 			<div className="mt-20">
 				<div className="translate-x-12 -translate-y-3 md:block hidden">
 					<img
+						alt="hanh-fuck"
 						src="/bean.png"
-						alt="cf-bean"
 						loading="lazy"
 						className={
 							visible
@@ -224,8 +225,8 @@ const Feature2 = () => {
 				</div>
 				<div className="translate-x-[-16em] -translate-y-8 md:hidden block -scale-x-100">
 					<img
+						alt="hanh-fuck"
 						src="/bean.png"
-						alt="cf-bean"
 						loading="lazy"
 						className={
 							visible
@@ -258,33 +259,34 @@ const Feature2 = () => {
 					<picture>
 						<div className="rotate-3">
 							<img
+								alt="hanh-fuck"
 								className={`transition-opacity duration-700 absolute z-1 scale-110 top-24 right-2 w-10 ${
 									visible
 										? 'animation-move-right'
 										: 'opacity-0'
 								}`}
 								src="https://www.nescafe.com/vn/themes/custom/nescafe/UI/build/images/3Lf_B7T.svg"
-								alt="line"
 								loading="lazy"
 							/>
 						</div>
 
 						<img
+							alt="hanh-fuck"
 							className={`transition-opacity duration-700 absolute left-[30em] z-0 w-44 bottom-0 ${
 								visible ? 'animation-move-right' : 'opacity-0'
 							}`}
 							src="/2.png"
-							alt="nông dân"
 							loading="lazy"
 						/>
 						<img
+							alt="hanh-fuck"
 							className="absolute left-[23em] top-[1em] rotate-3 h-80 w-52 z-[-2] border-4 border-white shadow-lg scale-125"
 							src="/anh-2.jpg"
-							alt="nông dân"
 							loading="lazy"
 						/>
 					</picture>
 					<img
+						alt="hanh-fuck"
 						className={`transition-opacity duration-700 absolute z-[-4] top-[-2em] left-[20em]  ${
 							visible ? 'animation-move-right' : 'opacity-0'
 						}`}
@@ -315,8 +317,8 @@ const Feature3 = () => {
 			<div className="-mt-10">
 				<div className="translate-x-36 md:block hidden">
 					<img
+						alt="hanh-fuck"
 						src="/bean.png"
-						alt="cf-bean"
 						loading="lazy"
 						className={
 							visible
@@ -340,8 +342,8 @@ const Feature3 = () => {
 				</div>
 				<div className="translate-x-[25em] -translate-y-16 md:hidden block">
 					<img
+						alt="hanh-fuck"
 						src="/bean.png"
-						alt="cf-bean"
 						loading="lazy"
 						className={
 							visible
@@ -374,33 +376,34 @@ const Feature3 = () => {
 					<picture>
 						<div className="rotate-90">
 							<img
+								alt="hanh-fuck"
 								className={`transition-opacity duration-700 absolute z-1 scale-110 top-24 right-60 w-10 ${
 									visible
 										? 'animation-move-right'
 										: 'opacity-0'
 								}`}
 								src="https://www.nescafe.com/vn/themes/custom/nescafe/UI/build/images/3Lf_B7T.svg"
-								alt="line"
 								loading="lazy"
 							/>
 						</div>
 
 						<img
+							alt="hanh-fuck"
 							className={`transition-opacity duration-700 absolute left-[3em] z-0 w-48 top-64 ${
 								visible ? 'animation-move-right' : 'opacity-0'
 							}`}
 							src="/3.png"
-							alt="nông dân"
 							loading="lazy"
 						/>
 						<img
+							alt="hanh-fuck"
 							className="absolute left-[-5em] top-0 h-80 w-56 z-[-2] border-4 border-white shadow-lg scale-110"
 							src="/anh-3.jpg"
-							alt="nông dân"
 							loading="lazy"
 						/>
 					</picture>
 					<img
+						alt="hanh-fuck"
 						className={`transition-opacity duration-700 absolute z-[-4] top-0 left-[-11em] w-80  ${
 							visible ? 'animation-move-right' : 'opacity-0'
 						}`}
@@ -431,8 +434,8 @@ const Feature4 = () => {
 			<div className="-mt-12">
 				<div className="translate-x-8 -translate-y-16 md:block hidden">
 					<img
+						alt="hanh-fuck"
 						src="/bean.png"
-						alt="cf-bean"
 						loading="lazy"
 						className={
 							visible
@@ -456,8 +459,8 @@ const Feature4 = () => {
 				</div>
 				<div className="-translate-x-[17em] -translate-y-36 md:hidden block -scale-x-100 ">
 					<img
+						alt="hanh-fuck"
 						src="/bean.png"
-						alt="cf-bean"
 						loading="lazy"
 						className={
 							visible
@@ -489,29 +492,30 @@ const Feature4 = () => {
 					</div>
 					<picture>
 						<img
+							alt="hanh-fuck"
 							className={`transition-opacity duration-700 absolute z-1 scale-110 top-28 right-8 w-10 ${
 								visible ? 'animation-move-right' : 'opacity-0'
 							}`}
 							src="https://www.nescafe.com/vn/themes/custom/nescafe/UI/build/images/3Lf_B7T.svg"
-							alt="line"
 							loading="lazy"
 						/>
 						<img
+							alt="hanh-fuck"
 							className={`transition-opacity duration-700 absolute left-[32em] z-0 w-48 top-[13em] ${
 								visible ? 'animation-move-right' : 'opacity-0'
 							}`}
 							src="/4.png"
-							alt="nông dân"
 							loading="lazy"
 						/>
 						<img
+							alt="hanh-fuck"
 							className="absolute left-[22em] top-[-2em] -rotate-4 h-72 w-52 z-[-2] border-4 border-white shadow-lg scale-125"
 							src="/anh-4.jpg"
-							alt="nông dân"
 							loading="lazy"
 						/>
 					</picture>
 					<img
+						alt="hanh-fuck"
 						className={`transition-opacity duration-700 absolute z-[-4] top-[-2em] left-[20em]  ${
 							visible ? 'animation-move-right' : 'opacity-0'
 						}`}
@@ -542,8 +546,8 @@ const Feature5 = () => {
 			<div className="-mt-28">
 				<div className="translate-x-36 md:block hidden">
 					<img
+						alt="hanh-fuck"
 						src="/bean.png"
-						alt="cf-bean"
 						loading="lazy"
 						className={
 							visible
@@ -567,8 +571,8 @@ const Feature5 = () => {
 				</div>
 				<div className="translate-x-[25em] -translate-y-20 md:hidden block ">
 					<img
+						alt="hanh-fuck"
 						src="/bean.png"
-						alt="cf-bean"
 						loading="lazy"
 						className={
 							visible
@@ -601,33 +605,34 @@ const Feature5 = () => {
 					<picture>
 						<div className="rotate-90">
 							<img
+								alt="hanh-fuck"
 								className={`transition-opacity duration-700 absolute z-1 scale-110 top-24 left-24 w-10 ${
 									visible
 										? 'animation-move-right'
 										: 'opacity-0'
 								}`}
 								src="https://www.nescafe.com/vn/themes/custom/nescafe/UI/build/images/3Lf_B7T.svg"
-								alt="line"
 								loading="lazy"
 							/>
 						</div>
 
 						<img
+							alt="hanh-fuck"
 							className={`transition-opacity duration-700 absolute left-6 z-0 w-52 top-60 ${
 								visible ? 'animation-move-right' : 'opacity-0'
 							}`}
 							src="/5.png"
-							alt="nông dân"
 							loading="lazy"
 						/>
 						<img
+							alt="hanh-fuck"
 							className="absolute left-[-6em] top-0 h-90 w-60 z-[-2] rotate-2 border-4 border-white shadow-lg scale-110"
 							src="/anh-5.jpg"
-							alt="nông dân"
 							loading="lazy"
 						/>
 					</picture>
 					<img
+						alt="hanh-fuck"
 						className={`transition-opacity duration-700 absolute z-[-4] top-[-2em] left-[-14em] w-80  ${
 							visible ? 'animation-move-left' : 'opacity-0'
 						}`}
